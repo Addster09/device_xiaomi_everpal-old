b1ed1d109b10cbba74b9d2c0aa6fff9fe8c10676
Addster09 <aadityasingh.cs9@gmail.com>
2026-02-12 16:28:06 +0530

    everpal: Cleanup!
    Signed-off-by: Addster09 <aadityasingh.cs9@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,25 +1,7 @@
 #
-# Copyright (C) 2023 The PixelExperience Project
-#
+# SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_everpal.mk \
-    $(LOCAL_DIR)/derp_everpal.mk \
-    $(LOCAL_DIR)/spark_everpal.mk \
     $(LOCAL_DIR)/lineage_everpal.mk
-
-COMMON_LUNCH_CHOICES := \
-    derp_everpal-user \
-    derp_everpal-userdebug \
-    derp_everpal-eng\
-    spark_everpal-user \
-    spark_everpal-userdebug \
-    spark_everpal-eng\
-    arrow_everpal-user \
-    arrow_everpal-userdebug \
-    arrow_everpal-eng \
-    lineage_everpal-user \
-    lineage_everpal-userdebug \
-    lineage_everpal-eng
